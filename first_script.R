9b19d93815e817ad96ddb839cd788fa1d198c83d
YongjunChu <ychu@smu.edu>
2019-04-04 15:06:51 -0500

    second commit

--- first_script.R
+++ first_script.R
@@ -2,3 +2,12 @@ a = 3
 b= 5
 c=a+b
 c
+
+#now let us create soem functions
+add <- function(a, b) { c=a+b
+                        d = a*b
+                        vec = c(c,d)
+                        return (vec)
+}
+
+add(4,7)
